f34b163a9c6f3c4d81b88ad9e9c98cf635f7c0a4
Nova Inal Zettergren <nova.inalzettergren@elev.ga.ntig.se>
2024-03-24 19:48:12 +0100

    skapat sista try-catchen för arrayen

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
-        
+
         try {
             System.out.println("Ange vad 10 ska delas med");
             int var1 = 10;
@@ -21,5 +21,16 @@ public class App {
         catch (Exception e) {
             System.out.println("Du skrev fel, du ska skriva med siffror");
         }
+
+        int[] fält = {6,3,5,9};
+        for (int i = 0; i < 10; i++) {
+        try {
+            System.out.println("element nr "+ i +" har värdet "+ fält[i]);
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Index "+ i +" är utanför fältets gränser.");
+        }
+
+        } 
     }
 }
