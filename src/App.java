ca680d331baaf52cc77c72fb5de8b303d4d91e93
Nova Inal Zettergren <nova.inalzettergren@elev.ga.ntig.se>
2024-03-24 19:45:40 +0100

    skapat ny try-catch med den nya kod

--- src/App.java
+++ src/App.java
@@ -3,15 +3,23 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Ange vad 10 ska delas med");
-        int var1 = 10;
         
         try {
+            System.out.println("Ange vad 10 ska delas med");
+            int var1 = 10;
             int var2 = scan.nextInt();
+        if (var2 == 0) {
+            throw new ArithmeticException("Du kan inte dela på 0");
+        }
+        double svar = ((double)var1) / var2;
+        System.out.println("10 delat med " + var2 +" är "+ svar);
+
         } 
+        catch (ArithmeticException e) {
+            System.out.println("Fel: "+ e.getMessage());
+        }
         catch (Exception e) {
             System.out.println("Du skrev fel, du ska skriva med siffror");
-            scan.nextLine();
         }
     }
 }
